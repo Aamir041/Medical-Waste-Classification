0e38ae6d3df72afed718944466d0b139cfc68417
Varun <83688812+varun07-cod@users.noreply.github.com>
2023-04-27 10:53:30 +0530

    Update App.js

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -3,14 +3,27 @@ import * as mobilenet from "@tensorflow-models/mobilenet";
 
 function App() {
     const [isModelLoading, setIsModelLoading] = useState(false)
+    //isModelLoading is a boolean that determines if the MobileNet model is still loading or not.
     const [model, setModel] = useState(null)
+    //model is the instance of the MobileNet model that will be used to identify objects in the image.
     const [imageURL, setImageURL] = useState(null);
+    //imageURL is the URL of the image that will be identified.
     const [results, setResults] = useState([])
+    //results is an array that will hold the results of the image identification
     const [history, setHistory] = useState([])
+    //history is an array that will hold the URLs of the recently uploaded images
 
     const imageRef = useRef()
+    //imageRef is a reference to the <img> tag that will display the uploaded image.
     const textInputRef = useRef()
+    //textInputRef is a reference to the input field where the user can enter the image URL.
     const fileInputRef = useRef()
+    //fileInputRef is a reference to the file input field where the user can upload an image.
+
+    //loadModel is an asynchronous function that loads the MobileNet model using 
+    //the mobilenet.load() method provided by the @tensorflow-models/mobilenet package. 
+    //It sets the model state to the loaded model instance and the isModelLoading state to false. If there is an error, 
+    //it logs the error to the console and sets the isModelLoading state to false.
 
     const loadModel = async () => {
         setIsModelLoading(true)
@@ -23,6 +36,8 @@ function App() {
             setIsModelLoading(false)
         }
     }
+    //uploadImage is a function that takes an event object as an argument and sets
+    //the imageURL state to the URL of the uploaded image. If there are no uploaded files, it sets the imageURL state to null.
 
     const uploadImage = (e) => {
         const { files } = e.target
@@ -33,26 +48,33 @@ function App() {
             setImageURL(null)
         }
     }
+    //identify is an asynchronous function that sets the results state to the results of 
+    //the MobileNet model's classify() method called on the imageRef reference. It also sets the textInputRef value to an empty string.
 
     const identify = async () => {
         textInputRef.current.value = ''
         const results = await model.classify(imageRef.current)
         setResults(results)
     }
-
+    
+    //handleOnChange is a function that takes an event object as an argument and sets 
+    //the imageURL state to the value of the text input field. It also sets the results state to an empty array.
     const handleOnChange = (e) => {
         setImageURL(e.target.value)
         setResults([])
     }
 
+    //triggerUpload is a function that triggers the click event of the fileInputRef reference.
     const triggerUpload = () => {
         fileInputRef.current.click()
     }
-
+    
+//The first useEffect hook runs the loadModel function when the component mounts.
     useEffect(() => {
         loadModel()
     }, [])
-
+    
+//The second useEffect hook runs whenever the imageURL state changes. If imageURL is truthy, it adds the URL to the beginning of the history state array.
     useEffect(() => {
         if (imageURL) {
             setHistory([imageURL, ...history])
