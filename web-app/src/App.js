2047d3dad37b35e7f37001c7825d144b9a971a6b
Aamir041 <aamirsaudagar34@gmail.com>
2023-04-28 19:54:20 +0530

    removed history array

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -10,8 +10,7 @@ function App() {
     //imageURL is the URL of the image that will be identified.
     const [results, setResults] = useState([])
     //results is an array that will hold the results of the image identification
-    const [history, setHistory] = useState([])
-    //history is an array that will hold the URLs of the recently uploaded images
+    
     const [notImage, setImgErr] = useState(false)
 
     const imageRef = useRef()
@@ -60,6 +59,7 @@ function App() {
             }
         }
         else {
+            setResults([]);
             setImageURL(null)
             setImgErr(true);
         }
@@ -99,11 +99,11 @@ function App() {
     }, [])
     
 //The second useEffect hook runs whenever the imageURL state changes. If imageURL is truthy, it adds the URL to the beginning of the history state array.
-    useEffect(() => {
-        if (imageURL) {
-            setHistory([imageURL, ...history])
-        }
-    }, [imageURL])
+    // useEffect(() => {
+    //     if (imageURL) {
+    //         setHistory([imageURL, ...history])
+    //     }
+    // }, [imageURL])
 
     if (isModelLoading) {
         return <h2>Model Loading...</h2>
@@ -113,7 +113,7 @@ function App() {
         <div className="App">
             <h1 className='header'>Image Identification</h1>
             <div className='inputHolder'>
-                <input type='file' accept='image/*' capture='camera' className='uploadInput' onChange={uploadImage} ref={fileInputRef} />
+                <input type='file' accept='image/*' capture='camera' className='uploadInput' id='input-img' onChange={uploadImage} ref={fileInputRef} />
                 <button className='uploadImage' onClick={triggerUpload}>Upload Image</button>
                 <span className='or'>OR</span>
                 <input type="text" placeholder='Paster image URL' ref={textInputRef} onChange={handleOnChange} />
@@ -137,18 +137,6 @@ function App() {
                 </div>
                 {imageURL && <button className='button' onClick={identify}>Identify Image</button>}
             </div>
-            {history.length > 0 && <div className="recentPredictions">
-                <h2>Recent Images</h2>
-                <div className="recentImages">
-                    {history.map((image, index) => {
-                        return (
-                            <div className="recentPrediction" key={`${image}${index}`}>
-                                <img src={image} alt='Recent Prediction' onClick={() => setImageURL(image)} />
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>}
         </div>
     );
 }
